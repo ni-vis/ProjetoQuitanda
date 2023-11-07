fcaaaa68521b7b28bd08b64c6ea40ee4d418c9b1
Nicole Vidal <nicole.silveira.senai@gmail.com>
2023-11-07 15:53:44 -0300

    Update ProjetoQuitanda

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import sqlite3 as sql
 import uuid
 # serve para gerar nomes/numeros aleatorios unicos para as imagens, gera numeros aleatorios para concatenar com os nomes das imagens que serao utilizadas (uuid)
 app = Flask(__name__)
-app.secrect_key = "quitandazezinho"
+app.secret_key = "quitandazezinho"
 # salvar sessao e colocar ums especie de segruança (secrect_key)
 usuario = 'Seu José'
 senha = 'Eu,Zé'
@@ -37,10 +37,25 @@ def iniciar_db():
 def index():
     iniciar_db()
     conexao = conecta_database()
-    produtos = conexao.execute('SELECT * FROM produtos ORDER BY id_prod DESC').fetchall
+    produtos = conexao.execute('SELECT * FROM produtos ORDER BY id_prod DESC').fetchall()
     conexao.close()
-    title = 'Home'
+    title = 'home'
     return render_template('home.html', produtos=produtos, title=title)
+# ROTA DA PÁGINA ACESSO ↧
+@app.route("/acesso", methods=['post'])
+#ROTA DA PÁGIA DE LOGIN↧
+@app.route("/login")
+def login():
+    return render_template("login.html")
+def acesso():
+    global usuario, senha
+    usuario_informado = request.form["usuario"]
+    senha_informada = request.form["senha"]
+    if usuario == usuario_informado and senha == senha_informada:
+        session["login"] = True
+        return redirect('/adm')
+    else:
+        return render_template("login.html",msg="Usuário/Senha estão incorretos!")
 # FINAL DO CODIGO - EXECUTANDO O SERVIDOR
 app.run(debug=True)
 # caso nao colocado não é possivel ver os erros
--- esquema.sql
+++ esquema.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS produtos (
     id_prod INTEGER PRIMARY KEY,
     -- nao pode cadastrar um produto no banco de dados sem ter um nome ↧
     nome_prod TEXT NOT NULL, 
+    desc_prod TEXT NOT NULL,
     -- nao pode cadastrar um produto no banco de dados sem ter um preço / REAL valor decimal (float) ↧
     preco_prod REAL NOT NULL,
     -- nao pode cadastrar um produto no banco de dados sem ter imagem / é text porque vai colocar endereço da imagem, é isso o que ele lê ↧
