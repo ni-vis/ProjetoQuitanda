65b5b1aa39bb4f4dcd342af25469fc95e64eeec7
Nicole Vidal <nicole.silveira.senai@gmail.com>
2023-11-10 15:29:08 -0300

    Update ProjetoQuitanda

--- app.py
+++ app.py
@@ -101,7 +101,8 @@ def cadastro():
         preco_prod=request.form['preco_prod']
         img_prod=request.files['img_prod']
         id_foto=str(uuid.uuid4().hex)
-        filename=id_foto+nome_prod+'png'
+        filename=id_foto+nome_prod+'.png'
+        img_prod.save('static/img/produtos/'+filename)
         conexao = conecta_database()
         conexao.execute('INSERT INTO produtos (nome_prod, desc_prod, preco_prod, img_prod) VALUES (?, ?, ?, ?)', (nome_prod, desc_prod, preco_prod, filename))
         conexao.commit()
@@ -109,8 +110,58 @@ def cadastro():
         return redirect('/adm')
     else:
         return redirect('/login')
-
-
+# ROTA DE EXCLUSÃO ↧
+@app.route('/excluir/<id>')
+def excluir(id):
+    if verifica_sessao():
+        id = int(id)
+        conexao = conecta_database()
+        conexao.execute('DELETE FROM produtos WHERE id_prod = ?', (id,))
+        conexao.commit()
+        conexao.close()
+        return redirect('/adm')
+    else: 
+        return redirect('/login')
+# ROTA DE EDIÇÃO ↧   
+@app.route('/editprodutos/<id_prod>')
+def editar(id_prod):
+    if verifica_sessao():
+        iniciar_db()
+        conexao = conecta_database()
+        produtos = conexao.execute('SELECT * FROM produtos WHERE id_prod = ?', (id_prod,)).fetchall()
+        conexao.close()
+        title = 'Edição de Produtos'
+        return render_template('editprodutos.html', produtos=produtos, title=title)
+    else:
+        return redirect('/login')
+# ROTA PARA TRATAR DA EDIÇÃO ↧
+@app.route('/editarprodutos', methods=['POST'])
+def editprod():
+    id_prod = request.form['id_prod']
+    nome_prod = request.form['nome_prod']
+    desc_prod = request.form['desc_prod']
+    preco_prod = request.form['preco_prod']
+    img_prod = request.files['img_prod']
+    
+    id_foto = str(uuid.uuid4().hex)
+    filename = id_foto + nome_prod + '.png'
+    
+    img_prod.save('static/img/produtos/' + filename)
+    
+    conexao = conecta_database()
+    conexao.execute('UPDATE produtos SET nome_prod = ?, desc_prod = ?, preco_prod = ?, img_prod = ? WHERE id_prod = ?', (nome_prod, desc_prod, preco_prod, filename, id_prod))
+    conexao.commit()
+    conexao.close()
+    
+    return redirect('/adm')
+# ROTA DA PÁGINA DE BUSCA
+@app.route('/busca', methods=['post'])
+def busca():
+    busca=request.form['buscar']
+    conexao = conecta_database()
+    produtos = conexao.execute('SELECT * FROM produtos WHERE nome_prod LIKE "%" || ? || "%"',(busca,)).fetchall()
+    title = "Home"
+    return render_template("home.html", produtos = produtos, title = title)
 # FINAL DO CODIGO - EXECUTANDO O SERVIDOR ↧
 app.run(debug=True)
 # caso nao colocado não é possivel ver os erros
